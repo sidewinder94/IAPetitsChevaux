4e7bbdca3137a1f888fc435bbaa5e511ada5bc4a
Antoine-Ali Zarrouk <antoineali.zarrouk@viacesi.fr>
2015-05-20 12:25:25 -0400

    Able to run many games in a row

--- WebSocketsTest/Program.cs
+++ WebSocketsTest/Program.cs
@@ -1,104 +1,53 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using Newtonsoft.Json;
-using SuperSocket.SocketBase;
+using System.Text.RegularExpressions;
+using Microsoft.ServiceModel.WebSockets;
 using PetitsChevaux.Game;
-using SuperWebSocket;
 
 namespace PetitsChevaux
 {
     class Program
     {
 
-        private static readonly Dictionary<WebSocketSession, Board> _sessions =
-            new Dictionary<WebSocketSession, Board>();
+
         static void Main(string[] args)
         {
+            int[] wins = { 0, 0 };
 
             Board.PlayerNumber = 2;
             Board.PawnsPerPlayer = 4;
 
-            var appServer = new WebSocketServer();
-
-            if (!appServer.Setup(10000))
+            while (wins.Sum() < 100000)
             {
-                Console.WriteLine("Failed to setup!");
-                Console.ReadKey();
-                return;
-            }
+                Console.WriteLine("[Player 1 : {0}, Player 2 : {1}] = {2}", wins[0], wins[1], wins.Sum());
+                Boolean run = true;
 
-            appServer.NewSessionConnected += AppServerOnNewSessionConnected;
-            appServer.SessionClosed += appServer_SessionClosed;
-            appServer.NewMessageReceived += appServer_NewMessageReceived;
+                Board.PlayerNumber = 2;
+                Board.PawnsPerPlayer = 4;
 
-            //Try to start the appServer
-            if (!appServer.Start())
-            {
-                Console.WriteLine("Failed to start!");
-                Console.ReadKey();
-                return;
-            }
-
-
-
-            Console.WriteLine("Server initialized");
-            Console.ReadLine();
-        }
-
-        private static void appServer_NewMessageReceived(WebSocketSession session, string value)
-        {
-            var board = _sessions[session];
-            //if (board.Players.Any(p => p.Won))
-            //{
-            //    board.GeneratePlayers();
-            //}
-
-            //int i = 100;
-            //board.Run = true;
-            //while (board.Run)
-            //{
-            //    session.Send(JsonConvert.SerializeObject(board.NextTurn()));
-
-            //    if (board.Players.Any(p => p.Won))
-            //    {
-            //        board.Run = false;
-            //        Console.WriteLine("{0} won !", board.Players.First(p => p.Won));
-            //    }
-
-            //    System.Threading.Thread.Sleep(i);
-            //}
-        }
-
-        static void appServer_SessionClosed(WebSocketSession session, CloseReason value)
-        {
-            _sessions.Remove(session);
-            Console.WriteLine("Session closed for {0}", value);
-        }
-
-        private static void AppServerOnNewSessionConnected(WebSocketSession session)
-        {
-            _sessions.Add(session, new Board());
-            var board = _sessions[session];
-            if (board.Players.Any(p => p.Won))
-            {
-                board.GeneratePlayers();
-            }
-
-            int i = 100;
-            board.Run = true;
-            while (board.Run)
-            {
-                session.Send(JsonConvert.SerializeObject(board.NextTurn()));
-
-                if (board.Players.Any(p => p.Won))
+                while (run)
                 {
-                    board.Run = false;
-                    Console.WriteLine("{0} won !", board.Players.First(p => p.Won));
+                    board.NextTurn();
+                    if (board.Players.Any(p => p.Won))
+                    {
+                        run = false;
+                        var winner = board.Players.First(p => p.Won);
+                        wins[winner.Id]++;
+                    }
                 }
 
-                System.Threading.Thread.Sleep(i);
+
             }
+            var results = String.Format("[Player 1 : {0}, Player 2 : {1}] = {2}", wins[0], wins[1], wins.Sum());
+            Console.WriteLine(results);
+            using (var fs = File.AppendText("results.txt"))
+            {
+                fs.Write(results + fs.NewLine);
+                fs.Flush();
+            }
+
+            Console.WriteLine("Finished");
+            Console.ReadLine();
+
         }
     }
 }
