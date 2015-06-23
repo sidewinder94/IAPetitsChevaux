1ede7d6471b614b7736127c20db146297f3c47d5
Antoine-Ali Zarrouk <antoineali.zarrouk@viacesi.fr>
2015-06-23 13:12:04 -0400

    added some command line arguments (port/IA selection)

--- WebSocketsTest/Program.cs
+++ WebSocketsTest/Program.cs
@@ -6,18 +6,27 @@ using Newtonsoft.Json;
 using PetitsChevaux.Contracts;
 using SuperSocket.SocketBase;
 using PetitsChevaux.Game;
+using PetitsChevaux.Plans;
+using PetitsChevaux.Plans.MiniMax;
 using SuperWebSocket;
 
 namespace PetitsChevaux
 {
     class Program
     {
+
+        private static int _port = 10000;
+        private static Func<Player, List<Player>, int, Contracts.Action> _nextMove = NegaMax.NextMove;
+
         static void Main(string[] args)
         {
 
+
+            ProcessArgs(args);
+
             var appServer = new WebSocketServer();
 
-            if (!appServer.Setup(10000))
+            if (!appServer.Setup(_port))
             {
                 Console.WriteLine("Failed to setup!");
                 Console.ReadKey();
@@ -36,10 +45,38 @@ namespace PetitsChevaux
                 return;
             }
 
-
-
             Console.WriteLine("Server initialized");
             Console.ReadLine();
+        }
+
+        private static void ProcessArgs(string[] args)
+        {
+            for (int i = 0; i < args.Length; i++)
+            {
+                switch (args[i])
+                {
+                    case "-p":
+                    case "-P":
+                        int tPort;
+                        var success = int.TryParse(args[++i], out tPort);
+                        if (success)
+                        {
+                            _port = tPort;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Port value can't be parsed : {0}", args[i]);
+                        }
+                        break;
+
+                    case "--simple":
+                        _nextMove = SimpleMinded.NextMove;
+                        break;
+                    default:
+                        Console.WriteLine("Unknown Param : {0}", args[i]);
+                        break;
+                }
+            }
         }
 
         private static void appServer_NewMessageReceived(WebSocketSession session, string value)
